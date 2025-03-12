816a48b8889aec471d7959fb103972c01766295a
MantaZvy <mantaszvy@gmail.com>
2025-03-12 23:16:55 +0000

    linked in django project as a collaborator, set up venv, created app(surveyPage) and structure(url routing and req).

--- mySkyProject/mySkyProject/urls.py
+++ mySkyProject/mySkyProject/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('welcomePage.urls')),
+    path('', include('surveyPage.urls')),
 ]
---
+++ mySkyProject/surveyPage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SurveypageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'surveyPage'
