861d80a4c975451744038291130e62e807472a49
MantaZvy <mantaszvy@gmail.com>
2025-04-18 13:31:04 +0100

    rearranged all 14 survey html pages, rearranged backend for connectivity

--- mySkyProject/surveyPage/urls.py
+++ mySkyProject/surveyPage/urls.py
@@ -4,4 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.survey, name="survey"),
+    path('survey/q2/', views.survey_q2, name='survey_q2'),
+    path('survey/q3/', views.survey_q3, name='survey_q3'),
+    path('survey/q4/', views.survey_q4, name='survey_q4'),
+    path('survey/q5/', views.survey_q5, name='survey_q5'),
+    path('survey/q6/', views.survey_q6, name='survey_q6'),
+    path('survey/q7/', views.survey_q7, name='survey_q7'),
+    path('survey/q8/', views.survey_q8, name='survey_q8'),
+    path('survey/q9/', views.survey_q9, name='survey_q9'),
+    path('survey/q10/', views.survey_q10, name='survey_q10'),
+    path('survey/q11/', views.survey_q11, name='survey_q11'),
+    path('survey/q12/', views.survey_q12, name='survey_q12'),
+    path('survey/q13/', views.survey_q13, name='survey_q13'),
+    path('survey/q14/', views.survey_q14, name='survey_q14'),
 ]
--- mySkyProject/surveyPage/views.py
+++ mySkyProject/surveyPage/views.py
@@ -4,3 +4,29 @@ from django.http import HttpResponse
 # Create your views here.
 def survey(request):
     return render(request, 'surveyQ1.html')
+def survey_q2(request):
+    return render(request, 'surveyQ2.html')
+def survey_q3(request):
+    return render(request, 'surveyQ3.html')
+def survey_q4(request):
+    return render(request, 'surveyQ4.html')
+def survey_q5(request):
+    return render(request, 'surveyQ5.html')
+def survey_q6(request):
+    return render(request, 'surveyQ6.html')
+def survey_q7(request):
+    return render(request, 'surveyQ7.html')
+def survey_q8(request):
+    return render(request, 'surveyQ8.html')
+def survey_q9(request):
+    return render(request, 'surveyQ9.html')
+def survey_q10(request):
+    return render(request, 'surveyQ10.html')
+def survey_q11(request):
+    return render(request, 'surveyQ11.html')
+def survey_q12(request):
+    return render(request, 'surveyQ12.html')
+def survey_q13(request):
+    return render(request, 'surveyQ13.html')
+def survey_q14(request):
+    return render(request, 'surveyQ14.html')
