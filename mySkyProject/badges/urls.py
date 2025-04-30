7a390e7ec4a9f101eba1993796234ef574dbbb64
MantaZvy <mantaszvy@gmail.com>
2025-04-30 16:38:53 +0100

    updated drop menu urls

--- mySkyProject/badges/urls.py
+++ mySkyProject/badges/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views 
 
+app_name = 'badges'
 
 urlpatterns = [
     path('', views.badges, name="badges"),
--- mySkyProject/dashboardPage/urls.py
+++ mySkyProject/dashboardPage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
 
+app_name = 'dashboard'  # Namespace for the app
 
 urlpatterns = [
     path('', views.dashboard, name="dashboardPage"),
