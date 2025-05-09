578e9361dd285796f7bf9ba4301f5d5ddf6e519b
Treya28 <165586119+Treya28@users.noreply.github.com>
2025-05-09 19:58:21 +0100

    fixed profile page and stylesheet

--- mySkyProject/profile/views.py
+++ mySkyProject/profile/views.py
@@ -7,8 +7,9 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def profile_view(request):
     return render(request, 'profile/profile.html')
 
